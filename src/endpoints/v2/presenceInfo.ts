0d050247c4d6b1ec0e5d632fe41d5ef1e75c68eb
TheGamerzs <44209534+TheGamerzs@users.noreply.github.com>
2020-07-06 20:24:21 +0100

    Update presenceInfo.ts

--- src/endpoints/v2/presenceInfo.ts
+++ src/endpoints/v2/presenceInfo.ts
@@ -1,19 +1,18 @@
 import { cache } from "../../index";
 import { RequestHandler } from "express";
 
-let prsInfo = cache.get("presenceInfo");
+let presenceInfo = cache.get("presenceInfo");
 
-cache.on("update", (_, data) => (prsInfo = data), {
+cache.on("update", (_, data) => (presenceInfo = data), {
 	only: "presenceInfo",
 });
 
 //* Request Handler
 const handler: RequestHandler = async (req, res) => {
-	//* If presence not set
 	if (!req.params["presence"]) return res.sendStatus(404);
 
 	return res.send(
-		prsInfo.filter((document) => document.name === req.params["presence"])
+		presenceInfo.filter((document) => document.name === req.params["presence"])
 	);
 };
 
