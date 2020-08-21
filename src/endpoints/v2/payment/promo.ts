21cfac8a27bdde7a1a6360e229f87125b6812463
TheGamerzs <44209534+TheGamerzs@users.noreply.github.com>
2020-08-21 19:27:23 +0100

    Update promo.ts

--- src/endpoints/v2/payment/promo.ts
+++ src/endpoints/v2/payment/promo.ts
@@ -16,34 +16,40 @@ const handler: RouteHandlerMethod<
 > = async (req, res) => {
 	if (!req.params["code"]) return res.send(404);
 
-	let userId: string;
+	let userPromo;
+	let promo;
+
 	if (req.params["token"]) {
 		getDiscordUser(req.params["token"]).then(async dUser => {
-			userId = dUser.id;
+			userPromo = await promotions.findOne({
+				code: req.params["code"],
+				userId: dUser.id
+			});
 		});
 	}
 
-	const promo = await promotions.findOne({
-		code: req.params["code"],
-		userId: null,
-	});
-	const userPromo = await promotions.findOne({
-		code: req.params["code"],
-		userId: userId,
+	promo = await promotions.findOne({
+		code: req.params["code"]
 	});
 
-	if (userPromo && userPromo.useLimit > 0 && userPromo.expire > Date.now()) {
-		return res.send(userPromo);
-	} else if (userPromo && userPromo.useLimit === 0) {
-		return res.send({ string: "checkout.maximumUses" });
-	} else if (userPromo && userPromo.expire <= Date.now()) {
-		return res.send({ string: "checkout.expiredCode" });
-	} else if (promo && promo.useLimit > 0 && promo.expire > Date.now()) {
-		return res.send(promo);
-	} else if (promo && promo.expire <= Date.now()) {
-		return res.send({ string: "checkout.expiredCode" });
-	} else if (promo && promo.useLimit === 0) {
-		return res.send({ string: "checkout.maximumUses" });
+	if (userPromo) {
+		if (userPromo.useLimit > 0 && userPromo.expires > Date.now()) {
+			return res.send(userPromo);
+		} else if (userPromo.useLimit === 0) {
+			return res.send({ string: "checkout.maximumUses" });
+		} else if (userPromo.expires <= Date.now()) {
+			return res.send({ string: "checkout.expiredCode" });
+		}
+	} else if (promo) {
+		if (promo.useLimit > 0 && promo.expires > Date.now()) {
+			return res.send(promo);
+		} else if (promo.expires <= Date.now()) {
+			return res.send({ string: "checkout.expiredCode" });
+		} else if (promo.useLimit === 0) {
+			return res.send({ string: "checkout.maximumUses" });
+		} else {
+			return res.send({ string: "checkout.invalidCode" });
+		}
 	} else {
 		return res.send({ string: "checkout.invalidCode" });
 	}
