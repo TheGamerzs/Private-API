d59fd969a12c7f9f017e1b043039b1fe84e21751
TheGamerzs <44209534+TheGamerzs@users.noreply.github.com>
2020-09-03 22:22:03 +0100

    Added Product Info API

---
+++ src/endpoints/v2/printful/productInfo.ts
@@ -0,0 +1,29 @@
+import { RouteGenericInterface, RouteHandlerMethod } from "fastify/types/route";
+import { IncomingMessage, Server, ServerResponse } from "http";
+import { pmdDB } from "../../../db/client";
+
+//* Define credits collection
+const merch = pmdDB.collection("merch");
+
+//* Request Handler
+const handler: RouteHandlerMethod<
+	Server,
+	IncomingMessage,
+	ServerResponse,
+	RouteGenericInterface,
+	unknown
+> = async (req, res) => {
+	if (!req.params["id"]) return res.send(404);
+
+	let items = [];
+	for (let id of req.params["id"].split(",")) {
+	items.push(await merch.findOne({
+		item_id: req.params["id"].split("-")[0]
+	}));
+}
+
+	res.send({ items });
+};
+
+//* Export handler
+export { handler };
