b01f4dd3d1bbd190c551a0be1ab2091eda0c2f15
TheGamerzs <44209534+TheGamerzs@users.noreply.github.com>
2020-09-13 16:12:26 +0100

    Update processTransaction.ts

--- src/endpoints/v2/payment/processTransaction.ts
+++ src/endpoints/v2/payment/processTransaction.ts
@@ -1,30 +1,51 @@
+import { RouteGenericInterface, RouteHandlerMethod } from "fastify/types/route";
+import { IncomingMessage, Server, ServerResponse } from "http";
 var braintree = require("braintree");
 
-var gateway = new braintree.BraintreeGateway({
+var gateway = braintree.connect({
 	environment: braintree.Environment.Sandbox,
 	merchantId: process.env.BrainTree_Merchant_Id,
 	publicKey: process.env.BrainTree_Public_Key,
-	privateKey: process.env.BrainTree_Private_Key,
+	privateKey: process.env.BrainTree_Private_Key
 });
 
-gateway.transaction.sale(
-	{
-		amount: "5.00",
-		paymentMethodNonce: "nonce-from-the-client",
-		options: {
-			submitForSettlement: true,
+const handler: RouteHandlerMethod<
+	Server,
+	IncomingMessage,
+	ServerResponse,
+	RouteGenericInterface,
+	unknown
+> = async (req, res) => {
+	gateway.transaction.sale(
+		{
+			amount: "5.00",
+			paymentMethodNonce: "nonce-from-the-client",
+			options: { submitForSettlement: false },
+			billing: {
+				firstName: "Paul",
+				lastName: "Smith",
+				company: "Braintree",
+				streetAddress: "1 E Main St",
+				extendedAddress: "Suite 403",
+				locality: "Chicago",
+				region: "IL",
+				postalCode: "60622",
+				countryCodeAlpha2: "US"
+			}
 		},
-	},
-	function (err, result) {
-		if (err) {
-			console.error(err);
-			return;
+		function (err, result) {
+			if (err) {
+				console.error(err);
+				return;
+			}
+			if (result.success) {
+				res.send("Transaction ID: " + result.transaction.id);
+			} else {
+				res.send(result.message);
+			}
 		}
+	);
+};
 
-		if (result.success) {
-			console.log("Transaction ID: " + result.transaction.id);
-		} else {
-			console.error(result.message);
-		}
-	}
-);
+//* Export handler
+export { handler };
